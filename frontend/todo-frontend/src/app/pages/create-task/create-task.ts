1f79b3feca1eefc6eefbaa7bd3de43f16e6451bc
MonaJani <ramona-schaefer1@web.de>
2026-04-11 22:12:19 +0200

    Created 3 of 4 pages

--- frontend/todo-frontend/src/app/pages/create-task/create-task.ts
+++ frontend/todo-frontend/src/app/pages/create-task/create-task.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-create-task',
-  imports: [],
+  standalone:true,
+  imports: [RouterLink],
   templateUrl: './create-task.html',
   styleUrl: './create-task.css',
 })
