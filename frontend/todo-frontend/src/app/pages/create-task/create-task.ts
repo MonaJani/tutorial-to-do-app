3f2c618deb456416bc8da1a8d39358c6fd5b102e
MonaJani <ramona-schaefer1@web.de>
2026-04-21 10:11:27 +0200

    Implemented futures

--- frontend/todo-frontend/src/app/pages/create-task/create-task.ts
+++ frontend/todo-frontend/src/app/pages/create-task/create-task.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import {Router, RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task';
 
 @Component({
   selector: 'app-create-task',
   standalone:true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './create-task.html',
   styleUrl: './create-task.css',
 })
-export class CreateTask {}
+export class CreateTask {
+  title: string = '';
+  description: string = '';
+  dueDate: string = '';
+  priority: 'low' | 'medium' |'high' = 'medium';
+
+  constructor(
+    private taskService: TaskService,
+    private router:Router
+
+  ) {}
+
+  createTask(): void {
+    if(!this.title.trim()) {
+      return;
+    }
+
+    const newTask: Task = {
+      id: Date.now(),
+      title: this.title,
+      description: this.description,
+      completed: false,
+      dueDate: this.dueDate,
+      priority: this.priority,
+    };
+
+    this.taskService.addTask(newTask);
+
+    this.title = '',
+    this.description = '';
+    this.dueDate = '',
+    this.priority = 'medium';
+
+    this.router.navigate(['/tasks']);
+
+  }
+
+
+}
